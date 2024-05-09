9ca1db56637ab1dde54186ac276dcfc17f3a7def
teogenes ferreira <teogenesster@gmail.com>
2024-05-09 00:59:55 -0300

    Update Contador.java

--- src/Contador.java
+++ src/Contador.java
@@ -11,12 +11,12 @@ public class Contador  {
        int parametroDois = terminal.nextInt();
        
       try {
-			//chamando o método contendo a lógica de contagem
+	   //chamando o método contendo a lógica de contagem
             contar(parametroUm, parametroDois);
 		
 		}catch (ParametrosInvalidosException  e) {
-			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
-            System.out.println("Erro: " + e.getMessage());
+		    //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+                     System.out.println("Erro: " + e.getMessage());
 		}
 
     }
@@ -26,11 +26,11 @@ public class Contador  {
         if(parametroUm > parametroDois){
             throw new ParametrosInvalidosException("O primeiro parametro não pode ser maior do que o segundo paramentro");
        }
-		int contagem = parametroDois - parametroUm;
+	   int contagem = parametroDois - parametroUm;
 		//realizar o for para imprimir os números com base na variável contagem
         for(int contador = 1; contador <= contagem; contador++ ){
                   System.out.println("Imprimindo o número " + contador);
        }
-	}
+   }
 
 }
